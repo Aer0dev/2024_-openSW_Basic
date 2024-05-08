5be0219c7c3e577fe573aa6d0009e9ce9b7c799b
ManaP0tion <45550827+Aer0dev@users.noreply.github.com>
2024-05-08 10:34:17 +0900

    test

--- Scenes/PlayerMovement.gd
+++ Scenes/PlayerMovement.gd
@@ -2,10 +2,23 @@ extends CharacterBody2D
 
 @export var Speed = 400
 
-func get_input():
-	var input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction * Speed
-		
-func _physics_process(delta):
-	get_input()
-	move_and_slide()
+func _ready():
+	screen_size = get_viewport_rect().size
+	
+
+func _process(delta):
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
