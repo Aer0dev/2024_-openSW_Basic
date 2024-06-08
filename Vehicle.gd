33229477d3dc30682c3ba5948f47cbe86ad30df8
Aer0dev <45550827+Aer0dev@users.noreply.github.com>
2024-06-08 15:52:51 +0900

    0608
    자동차 랜덤스폰
    플레이어 무브먼트 변경

--- Vehicle.gd
+++ Vehicle.gd
@@ -1,10 +1,29 @@
 extends KinematicBody
 
 var velocity := Vector3.ZERO
+var car_list:Array = []
+
+func add_list():
+	var dir = Directory.new()
+	dir.open("res://Assets/cars/")
+	dir.list_dir_begin()
+	
+	while true:
+		var file : String = dir.get_next()
+		if file == "":
+			break
+		elif file.ends_with(".vox"):
+			car_list.append(file)
+	
+	dir.list_dir_end()
+	print(car_list)
 
 func start(speed:float) -> void:
+	add_list()
 	velocity = Vector3(speed, 0, 0)
 	$MeshInstance.rotation_degrees.y = 0.0 if sign(speed) == 1 else 180.0
+	$MeshInstance.mesh = load("res://Assets/cars/" + car_list[randi()%car_list.size()])
+
 func _physics_process(delta:float)-> void:
 	velocity = move_and_slide(velocity, Vector3.UP)
 	
--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,26 +1,39 @@
 extends Area
 
 var is_moving := false
+var is_rotating := false
+var changeScale := false
+
 var dir := Vector3.ZERO
 
 func _physics_process(delta: float)->void:
+	var r0 :float= $MeshInstance.rotation_degrees.y
+	var r1 :float= $MeshInstance.rotation_degrees.y
+	
 	dir = Vector3.ZERO
 	
 	if Input.is_action_just_pressed("ui_up"):
+		if r0 != 0.0: r1 = 0.0
 		if not $ray_up.is_colliding():
 			 dir = Vector3.BACK
 	elif Input.is_action_just_pressed("ui_right"):
+		if r0 != -90: r1 = -90
 		if $ray_left.is_colliding() == false:
 			dir = Vector3.LEFT
 	elif Input.is_action_just_pressed("ui_left"):
+		if r0 != 90: r1 = 90
 		if $ray_right.is_colliding() == false:
 			dir = Vector3.RIGHT
 	
 	if dir == Vector3.BACK:
-		get_parent().z_player = int(self.global_translation.z)
+		get_parent().z_player = int(self.translation.z)
 		
-	
-	print(dir)
+	if r0 != r1 and not is_rotating:
+		$tw_r.interpolate_property($MeshInstance, "rotation_degrees:y", r0, r1, 0.1,Tween.TRANS_EXPO, Tween.EASE_OUT)
+		$tw_r.start()
+		yield($tw_r, "tween_all_completed")
+		is_rotating = false
+
 	
 	if dir != Vector3.ZERO and not is_moving:
 		is_moving = true
@@ -35,4 +48,12 @@ func _physics_process(delta: float)->void:
 		yield($tw, "tween_all_completed")
 		is_moving = false
 	
-	
+	if not changeScale:
+		changeScale = true
+		var scale1 = Vector3.ONE
+		var scale2 = Vector3(1.1, 0.9, 1.1)
+		$tw_b.interpolate_property($MeshInstance, "scale", scale1, scale2, 0.2, Tween.TRANS_SINE, Tween.EASE_IN)
+		$tw_b.interpolate_property($MeshInstance, "scale", scale2, scale1, 0.2, Tween.TRANS_SINE, Tween.EASE_IN)
+		$tw_b.start()
+		yield($tw_b, "tween_all_completed")
+		changeScale = false
